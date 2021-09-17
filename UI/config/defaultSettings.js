572279f15c5f065e18ab4e193450463ed87ced33
qihang1 <87305511+qihang1@users.noreply.github.com>
2021-09-17 17:43:31 +0800

    First commit

---
+++ UI/config/defaultSettings.js
@@ -0,0 +1,18 @@
+export default {
+  navTheme: 'light',
+  primaryColor: '#1890FF',
+  layout: 'topmenu',
+  contentWidth: 'Fluid',
+  fixedHeader: true,
+  autoHideHeader: true,
+  fixSiderbar: false,
+  colorWeak: false,
+  menu: {
+    locale: false,
+  },
+  // title: 'Interface Auto Test',
+  title: '自动化测试平台',
+
+  pwa: false,
+  iconfontUrl: '',
+};
